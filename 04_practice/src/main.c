72c79efd1b44cfed1483794181033d94c82eb094
martin <lmapii@gmail.com>
2023-11-01 11:21:01 +0100

    added launch.json for cortex-m

--- 04_practice/src/main.c
+++ 04_practice/src/main.c
@@ -1,17 +1,29 @@
 /** \file main.c */
 
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 
-#define SLEEP_TIME_MS 100U
+#define LED_NODE DT_CHOSEN(app_led)
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+
+#define SLEEP_TIME_MS 500U
 
 void main(void)
 {
-    printk("Message in a bottle.\n");
+    if (!gpio_is_ready_dt(&led))
+    {
+        return;
+    }
 
-    // TODO: MEMFAULT_ASSERT() for failed status
+    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) != 0)
+    {
+        return;
+    }
 
     while (1)
     {
+        (void) gpio_pin_toggle_dt(&led);
         k_msleep(SLEEP_TIME_MS);
     }
 }
